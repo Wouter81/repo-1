45b6b2591bfda0ca9f8aed8aee8fbd7cd624adbf
DOKI <DOKI>
2016-09-08 22:59:18 +0200

    DOKI

--- plugin.video.dutchmovies/default.py
+++ plugin.video.dutchmovies/default.py
@@ -11,11 +11,29 @@ icon = local.getAddonInfo('icon')
 FANART = local.getAddonInfo('fanart')
 
 channellist=[
-        ("Nederlandse films", "playlist/PLwZj5jiLW6kxWVZJAocTrkjZn_z5aU17R", 'https://www.mupload.nl/img/yh3dab22pno80.png'),
-		("Films met ingebakken subs", "playlist/PLwZj5jiLW6kzdzaSR3iX_t6M6UaI_QHMO", 'https://www.mupload.nl/img/yh3dab22pno80.png'),	
+        ("[COLOR darkorange]Nederlandse films[/COLOR]", "playlist/PLwZj5jiLW6kxWVZJAocTrkjZn_z5aU17R", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Films met ingebakken subs[/COLOR]", "playlist/PLwZj5jiLW6kzdzaSR3iX_t6M6UaI_QHMO", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]YT Kanaal: PolitieNostalgie[/COLOR]", "user/PolitieNostalgie", "https://yt3.ggpht.com/-WPXk_n6TycA/AAAAAAAAAAI/AAAAAAAAAAA/p8OxAn9eylY/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"),
+		
+		("[COLOR darkorange]YT Kanaal: Tv Series[/COLOR]", "channel/UClOL0I_2CsJTBd3Pfd2yrGw", "https://www.mupload.nl/img/nkskln9iwquob.jpg"),
+		
+		("[COLOR darkorange]Nederlandse serie's: Toen was geluk heel gewoon[/COLOR]", "playlist/PL93_4h9baNvOKPSwNPLc0ye9agfteowfq", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie's: Unit 13[/COLOR]", "playlist/PLoNJZg83zwjF8CI_VmY2D5BBMg607OF48", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: Van Speijk[/COLOR]", "playlist/PLwZj5jiLW6kwN9WignsLB5KP-Z1PPDaVM", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: Bureau Kruislaan[/COLOR]", "playlist/PLwZj5jiLW6kxkyGxz3mQiL6nH70-sW1lP", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: Grijpstra & De Gier[/COLOR]", "playlist/PLwZj5jiLW6kw5W_8Lv4HSMFbP46i45h9o", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: Combat[/COLOR]", "playlist/PL1B6577408F8038C7", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		
 		
         ]
-
 
 
 
