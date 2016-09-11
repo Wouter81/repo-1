da2d1e1b87bf3bbef21bb9fb68de86f824fa5f98
RickGAE <rick.nlv@hotmail.com>
2016-09-11 21:07:52 +0200

    Update

--- plugin.video.dutchmovies/default.py
+++ plugin.video.dutchmovies/default.py
@@ -13,6 +13,10 @@ FANART = local.getAddonInfo('fanart')
 channellist=[
         ("[COLOR darkorange]Nederlandse films[/COLOR]", "playlist/PLwZj5jiLW6kxWVZJAocTrkjZn_z5aU17R", "https://www.mupload.nl/img/yh3dab22pno80.png"),
 		
+		("[COLOR darkorange]Nederlandse Kinderen & Jeugdfilms[/COLOR]", "playlist/PLwZj5jiLW6kz4CRhpDU_bU-kJ_TP03x8x", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlands Gesproken tekenfilms[/COLOR]", "playlist/PLwZj5jiLW6kzEmBuWEh-KzlREFQC7HzRb", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
 		("[COLOR darkorange]Films met ingebakken subs[/COLOR]", "playlist/PLwZj5jiLW6kzdzaSR3iX_t6M6UaI_QHMO", "https://www.mupload.nl/img/yh3dab22pno80.png"),
 		
 		("[COLOR darkorange]YT Kanaal: PolitieNostalgie[/COLOR]", "user/PolitieNostalgie", "https://yt3.ggpht.com/-WPXk_n6TycA/AAAAAAAAAAI/AAAAAAAAAAA/p8OxAn9eylY/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"),
@@ -31,6 +35,17 @@ channellist=[
 		
 		("[COLOR darkorange]Nederlandse serie: Combat[/COLOR]", "playlist/PL1B6577408F8038C7", "https://www.mupload.nl/img/yh3dab22pno80.png"),
 		
+		("[COLOR darkorange]Nederlandse serie: All Stars[/COLOR]", "playlist/PLwZj5jiLW6kzaP1egWfCnTO0C6UpTzrHK", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: De Co-assistent[/COLOR]", "playlist/PLwZj5jiLW6kyf8StMDvPi-mEIQeNuRQdz", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: Vrienden voor het leven[/COLOR]", "playlist/PLwZj5jiLW6kysrSzw6CVDzCIPTb2-lZDf7", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: De Brekers[/COLOR]", "PLwZj5jiLW6kztZG0l1NfcNrpBV6O8FPOr", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: SamSam[/COLOR]", "playlist/PLwZj5jiLW6kwh8tYM2mKjNqRW7VVmBWGW", "https://www.mupload.nl/img/yh3dab22pno80.png"),
+		
+		("[COLOR darkorange]Nederlandse serie: MTV The Trip (S1)[/COLOR]", "playlist/PLwZj5jiLW6kwqVvfHxO1IdaX8VocbhkpA", "https://www.mupload.nl/img/yh3dab22pno80.png"),
 		
 		
         ]
